0a82f7ef38ad91ae3bea535eb681869ed535e4f0
Sven Johansson <sven.johansson@iteam.se>
2025-10-25 21:33:37 +0200

    Added dired command bound to (kbd ",") to jump to file by first char in file name

---
+++ emacs/config-dired.el
@@ -0,0 +1,32 @@
+;;; config-dired.el --- Early initialization -*- lexical-binding: t; -*-
+
+
+
+(defun dired-jump-to-first-file-by-letter ()
+  "Jump to the first file in Dired whose name starts with a given letter."
+  (interactive)
+  (let ((char (read-char "Jump to file starting with: "))
+        (case-fold-search t))
+    (goto-char (point-min))
+    (unless (re-search-forward "\\.\\." nil t)
+      (re-search-forward "\\." nil t))
+
+    (re-search-backward " " nil t)
+
+    (let ((found nil)
+          (col (1+ (current-column))))
+      (while (and (null found)
+                  (re-search-forward (format  " %c" char) nil t))
+        (setq found (not (< (current-column) col)))))
+    (dired-move-to-filename)))
+
+
+;; Key Bindings
+
+(define-key dired-mode-map (kbd ",") #'dired-jump-to-first-file-by-letter)
+
+
+
+(provide 'config-dired)
+
+;;; config-dired.el ends here
--- emacs/emacs-config.el
+++ emacs/emacs-config.el
@@ -2,6 +2,7 @@
 
 (require 'config-secrets)
 (require 'config-global)
+(require 'config-dired)
 (require 'config-keymaps)
 (require 'config-mouse)
 (require 'config-emacs-files)
