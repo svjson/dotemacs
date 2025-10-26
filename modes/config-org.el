e81bb4380155784d25637e21f788b4afd2c91376
Sven Johansson <sven.johansson@iteam.se>
2025-10-26 19:29:36 +0100

    Override date navigation key in org-read-date-minibuffer-local-map so they don't collide with windmove

--- modes/config-org.el
+++ modes/config-org.el
@@ -2,12 +2,24 @@
 
 (require 'org)
 
+
+
 (setq org-todo-keywords
       '((sequence "TODO" "ONGOING" "PAUSED" "|" "DONE")))
 
 (setq org-todo-keyword-faces
       '(("ONGOING" . "orange")
         ("PAUSED" . "yellow")))
+
+;; Key Bindings
+
+
+(with-eval-after-load 'org
+  (let ((map org-read-date-minibuffer-local-map))
+    (define-key map (kbd "s-<down>")  #'org-calendar-forward-week)
+    (define-key map (kbd "s-<up>")    #'org-calendar-backward-week)
+    (define-key map (kbd "s-<left>")  #'org-calendar-backward-day)
+    (define-key map (kbd "s-<right>") #'org-calendar-forward-day)))
 
 (define-key org-mode-map (kbd "s-<right>") #'org-shiftright)
 (define-key org-mode-map (kbd "s-<left>") #'org-shiftleft)
