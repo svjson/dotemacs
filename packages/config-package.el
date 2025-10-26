58f8fae9961ce773701f820bea7ddb95ac42315b
Sven Johansson <sven.johansson@iteam.se>
2025-10-26 23:26:27 +0100

    Always defer package loading

--- packages/config-package.el
+++ packages/config-package.el
@@ -12,7 +12,8 @@
 ;; Load use-package
 (eval-when-compile
   (require 'use-package))
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure t
+      use-package-always-defer t)
 
 (provide 'config-package)
 
