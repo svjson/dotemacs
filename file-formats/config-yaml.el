bb36d0c62035e752bb7742c112f20e23f70ef6af
Sven Johansson <sven.johansson@iteam.se>
2025-11-15 23:51:01 +0100

    Avoid pre-load yaml-ts-mode

--- file-formats/config-yaml.el
+++ file-formats/config-yaml.el
@@ -1,15 +1,12 @@
 ;;; config-yaml.el --- yaml mode editing -*- lexical-binding: t; -*-
 
-(require 'yaml-ts-mode)
-
-(add-to-list 'auto-mode-alist '("\\*.yaml" . yaml-ts-mode))
-
-(add-hook 'yaml-ts-mode-hook
-          (lambda ()
-            (setq show-trailing-whitespace t)))
-
-(define-key yaml-ts-mode-map (kbd "M-m d") #'base64-decode-region)
-(define-key yaml-ts-mode-map (kbd "M-m e") #'base64-encode-region)
+(use-package yaml-ts-mode
+  :straight (:type built-in)
+  :mode (("\\.ya?ml\\'" . yaml-ts-mode))
+  :hook (yaml-ts-mode . (lambda () (setq show-trailing-whitespace t)))
+  :config
+  (define-key yaml-ts-mode-map (kbd "M-m d") #'base64-decode-region)
+  (define-key yaml-ts-mode-map (kbd "M-m e") #'base64-encode-region))
 
 (provide 'config-yaml)
 
