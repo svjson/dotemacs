a02066f89eb6820dc80679fe2802b2c29e62ca9d
Sven Johansson <johansson.sven@gmail.com>
2025-11-29 13:43:28 +0100

    config-elisp: Moar unbind functions, unbinding all symbols with a certain prefix in one fell swoop

--- lang/emacs-lisp/config-elisp-symbol-bindings.el
+++ lang/emacs-lisp/config-elisp-symbol-bindings.el
@@ -5,26 +5,34 @@
 (defun svjson/delete-face-at-point ()
   "Delete/undef the face at point so it can be redefined with `defface`."
   (interactive)
-  (let* ((sym (symbol-at-point))
-         (face (and (symbolp sym) (facep sym) sym)))
-    (if (not face)
-        (message "No face at point.")
-      ;; Remove from internal face storage
-      (face-spec-set face nil)
-      (put face 'face-defface-spec nil)
-      (message "Face `%s` deleted. You can now redefine it." face))))
+  (svjson/el/delete-face (symbol-at-point)))
 
-(defun svjson/unbind-symbol-at-point ()
-  (interactive)
-  (let ((symbol (thing-at-point 'symbol)))
+(defun svjson/el/delete-face (face-symbol)
+  (if-let ((face (and (symbolp face-symbol) (facep face-symbol) face-symbol)))
+      (progn
+        (face-spec-set face nil)
+        (put face 'face-defface-spec nil)
+        (message "Face `%s` deleted. You can now redefine it." face))
+    (message "No face at point.")))
+
+(defun svjson/el/unbind-symbol (symbol)
+  (let ((symbol (if (symbolp symbol) symbol (intern symbol))))
     (cond
+     ((fboundp symbol)
+      (fmakunbound symbol))
+
      ((facep symbol)
-      (svjson/delete-face-at-point))
+      (svjson/el/delete-face symbol))
 
      (t
       (progn
-        (makunbound (intern symbol))
-        (message "Unbound symbol '%s" (intern symbol)))))))
+        (makunbound symbol)
+        (message "Unbound symbol '%s" symbol))))))
+
+(defun svjson/unbind-symbol-at-point ()
+  (interactive)
+  (when-let ((symbol (thing-at-point 'symbol)))
+    (svjson/el/unbind-symbol symbol)))
 
 (defun svjson/nuke-defface-from-orbit (face)
   "Remove all specs and properties for FACE, even if it was defined incorrectly."
@@ -37,6 +45,64 @@
     (put face 'saved-face nil)
     (put face 'customized-face-comment nil)
     (message "Face `%s` forcibly reset. You may now re-`defface` it." face)))
+
+(defun svjson/el/extract-prefix (symbol &optional exclude-delimiter)
+  "Extract the prefix part of a symbol."
+  (let ((name (symbol-name symbol)))
+    (when (or (string-match "--" name)
+              (string-match "/" name))
+      (substring name 0 (if exclude-delimiter (match-beginning 0) (match-end 0))))))
+
+(defun svjson/el/buffer-prefixes ()
+  "Find all symbol prefixes present in buffer declarations."
+  (save-excursion
+    (goto-char (point-min))
+    (let (prefixes form)
+      (while (setq form (ignore-errors (read (current-buffer))))
+        (when (memq (car form)
+                    (list 'cl-defun
+                          'cl-defgeneric
+                          'cl-defmethod
+                          'defconst
+                          'defcustom
+                          'defface
+                          'defmacro
+                          'defun
+                          'defvar
+                          'ert-deftest))
+          (when-let ((prefix (svjson/el/extract-prefix (cadr form))))
+            (unless (member prefix prefixes)
+              (push prefix prefixes)))))
+      prefixes)))
+
+(defun svjson/el/prefix-at-point-or-file (&optional prefixes)
+  "Get the symbol prefix at point or fall back on PREFIXES or file name."
+  (if-let ((sym (symbol-at-point)))
+      (svjson/el/extract-prefix sym)
+    (or (car prefixes) (file-name-base (buffer-file-name)))))
+
+(defun svjson/el/symbols-with-prefix (prefix)
+  "Find all declared symbols starting with PREFIX in the global ns."
+  (let (symbols)
+    (mapatoms
+     (lambda (sym)
+       (when (string-prefix-p prefix (symbol-name sym))
+         (push sym symbols))))
+    symbols))
+
+(defun svjson/el/unbind-all-with-prefix (&optional prefix)
+  "Unbind all symbols starting with PREFIX."
+  (interactive)
+  (let* ((buffer-prefixes (svjson/el/buffer-prefixes))
+         (prefix (or prefix
+                     (completing-read "Unbind symbols with prefix: "
+                                      buffer-prefixes
+                                      nil
+                                      nil
+                                      (svjson/el/prefix-at-point-or-file buffer-prefixes))))
+         (symbols (svjson/el/symbols-with-prefix prefix)))
+    (dolist (sym symbols)
+      (svjson/el/unbind-symbol sym))))
 
 
 
