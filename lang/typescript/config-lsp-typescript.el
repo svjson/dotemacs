63a21745b3892701876d1038fbf97cca0fb0be3c
Sven Johansson <johansson.sven@gmail.com>
2025-12-18 14:52:36 +0100

    config-lsp-typescript: Disable aggressive(and incorrect) format on save via LSP

--- lang/typescript/config-lsp-typescript.el
+++ lang/typescript/config-lsp-typescript.el
@@ -1,9 +1,25 @@
 ;;; config-lsp-typescript.el --- -*- lexical-binding: t; -*-
 
+
+
+;; Forward declarations
+(declare-function lsp--cur-workspace "lsp-mode")
+(declare-function lsp-organize-imports "lsp-mode")
+(defvar lsp-clients-typescript-preferences nil)
+
+
+;; Variables
+(setq lsp-clients-typescript-preferences
+      '(:formatOnSave false))
+
+
+
 (defun svjson/lsp-organize-imports-if-connected ()
   (when (and (require 'lsp-mode nil t)
              (bound-and-true-p lsp-mode)
              (lsp--cur-workspace)) ;; or (lsp-workspaces) if using multiple
     (lsp-organize-imports)))
+
+
 
 ;;; config-lsp-typescript ends here
