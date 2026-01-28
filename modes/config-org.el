4dbd995403daab731e4fdd43e7c5ee77881b41ec
Sven Johansson <johansson.sven@gmail.com>
2026-01-28 20:08:15 +0100

    config-org: More ergonomic TODO cycling/setting

--- modes/config-org.el
+++ modes/config-org.el
@@ -4,6 +4,7 @@
 (require 'org-agenda)
 
 
+;; org general
 
 (setq org-todo-keywords
       '((sequence "TODO" "ONGOING" "PAUSED" "|" "DONE")))
@@ -14,12 +15,8 @@
 
 (setq org-tags-column -80)
 
-(setq org-agenda-skip-scheduled-if-deadline-is-shown nil)
-(setq org-agenda-skip-deadline-if-done nil)
-(setq org-agenda-skip-scheduled-if-done nil)
-(setq org-scheduled-past-days 99999)
 
-;; Key Bindings
+;; Key Bindings (org general)
 
 
 (with-eval-after-load 'org
@@ -31,6 +28,44 @@
 
 (define-key org-mode-map (kbd "s-<right>") #'org-shiftright)
 (define-key org-mode-map (kbd "s-<left>") #'org-shiftleft)
+
+
+
+;; org-agenda
+
+(setq org-agenda-skip-scheduled-if-deadline-is-shown nil)
+(setq org-agenda-skip-deadline-if-done nil)
+(setq org-agenda-skip-scheduled-if-done nil)
+(setq org-scheduled-past-days 99999)
+
+(defun svjson/org-todo->TODO ()
+  (interactive)
+  (org-agenda-todo "TODO"))
+
+(defun svjson/org-todo->ONGOING ()
+  (interactive)
+  (org-agenda-todo "ONGOING"))
+
+(defun svjson/org-todo->PAUSED ()
+  (interactive)
+  (org-agenda-todo "PAUSED"))
+
+(defun svjson/org-todo->DONE ()
+  (interactive)
+  (org-agenda-todo "DONE"))
+
+
+;; Key Bindings (org-agenda)
+
+
+(with-eval-after-load 'org-agenda
+  (define-key org-agenda-mode-map (kbd "C-c <backspace>") #'svjson/org-todo->TODO)
+  (define-key org-agenda-mode-map (kbd "C-c o") #'svjson/org-todo->ONGOING)
+  (define-key org-agenda-mode-map (kbd "C-c p") #'svjson/org-todo->PAUSED)
+  (define-key org-agenda-mode-map (kbd "C-c SPC") #'svjson/org-todo->DONE))
+
+
+
 
 (provide 'config-org)
 
